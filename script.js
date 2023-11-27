9f72aa01659f6b734a23b7ca10326abd44ece245
krishna9809 <multiatoms980996@gmail.com>
2023-11-27 11:07:55 +0530

    Watch Website

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function toggleAnswer(answerId, iconId) {
   var menu = document.querySelector('.menu');
   menu.classList.toggle('show');
 }
+
+var crsr = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",function(dets) {
+    crsr.style.left = dets.x + -15 + "px";
+    crsr.style.top = dets.y +-5 + "px";
+})
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Set up GSAP timeline
+    var tl = gsap.timeline();
+
+    // Animation for the main content
+    tl.from(".about-section", { opacity: 0, duration: 1, ease: "power2.inOut" });
+    tl.from(".testimonials-section", { opacity: 0, duration: 4, ease: "power2.inOut" });
+
+  });
+
